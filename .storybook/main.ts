0ec09341215e271343677011b8e63aa875c17d4c
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2024-12-16 14:51:29 +0100

    setup storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,32 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: ['../**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-themes',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+  ],
+  logLevel: 'error',
+  // enable if needed
+  // staticDirs: ['../public'],
+  typescript: { reactDocgen: false, check: false },
+  core: { disableTelemetry: true, disableWhatsNewNotifications: true },
+  framework: {
+    name: '@storybook/nextjs',
+    options: { builder: { useSWC: true } },
+  },
+  previewBody: `<style>:root { color-scheme: light; } html[data-theme="dark"] { color-scheme: dark; }
+      #storybook-root { height: 100vh; width: 100vw; }
+      body {
+        @apply min-h-screen
+        bg-white
+        text-gray-950
+        dark:bg-gray-950
+        dark:text-gray-50;
+      }
+    </style>`,
+};
+export default config;
