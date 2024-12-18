eb486ee0d0f42a2ad33214c04479cd1ee04c844a
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2024-12-18 17:08:39 +0100

    wip

---
+++ components/Landing/index.tsx
@@ -0,0 +1,13 @@
+import Features from './Features/index.tsx';
+import Hero from './Hero/index.tsx';
+import type { FC } from 'react';
+
+
+const Landing: FC = () => (
+    <>
+    <Hero />
+    <Features />
+    </>
+);
+
+export default Landing;
